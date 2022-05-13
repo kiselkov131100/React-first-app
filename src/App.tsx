602740dbdfd36e4b16dc8bb727f9eeb56a0a6dd6
Ivan Kiselkov <ivan131100@icloud.com>
2022-05-13 14:26:10 +0300

    fix bugs

--- src/App.tsx
+++ src/App.tsx
@@ -9,60 +9,22 @@ import Template from './pages/Template'
 import HeaderPages from "./components/HeaderPages";
 import { ThemeModeProvider } from "./context/ThemeModeProvider";
 import { Theme } from "./context/ThemeModeContext";
+import Router from "./pages/Router";
 
 function App() {
   const [theme, setTheme] = useState(Theme.Light);
+
   const onChangeTheme = (value: Theme) => {
     setTheme(value);
   };
 
-  const MOCK_DATA = [
-    {
-      id: 1,
-      image: "",
-      text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-      date: "2022-01-02",
-      lesson_num: 0,
-      title: "What is Lorem ipsum?",
-      author: 0,
-    },
-    {
-      id: 2,
-      image: "",
-      text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-      date: "2022-02-03",
-      lesson_num: 0,
-      title: "What is Lorem ipsum?",
-      author: 0,
-    },
-    {
-      id: 3,
-      image: "",
-      text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-      date: "2022-03-04",
-      lesson_num: 0,
-      title: "What is Lorem ipsum?",
-      author: 0,
-    },
-    {
-      id: 4,
-      image: "",
-      text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-      date: "2021-04-05",
-      lesson_num: 0,
-      title: "What is Lorem ipsum?",
-      author: 0,
-    },
-  ];
-
   return (
- <ThemeModeProvider theme={theme} onChangeTheme={onChangeTheme}>
-   <div className="App">
-   <CardPosts data={MOCK_DATA} />
-  {/* <Authorization />     */}
-  </div>
-  </ThemeModeProvider>
-    );
-  };
+    <ThemeModeProvider theme={theme} onChangeTheme={onChangeTheme}>
+      <div className="App">
+        <Router />
+      </div>
+    </ThemeModeProvider>
+  );
+}
 
 export default App;
